e466bb9117a2fe1291830e5b3aea419d7cfb185d
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-03 12:18:14 +0530

    bigInt datatype example

---
+++ JS class/Day-3/Day -3.js
@@ -0,0 +1,26 @@
+
+// Bigint datatype addtion 
+
+function show() {
+  // let num =45;
+  // let str = "ashafak";
+
+  // let ansnum = typeof(num);
+  // let ansstr = typeof (str);
+
+  // let output = ansnum == ansstr
+
+  // console.log(output);
+
+  let num = 20000000000045346346345343455n;
+  let output = typeof num;
+  console.log(output);
+
+  let num1 = 20000000000045346346345343455n;
+  let output1 = typeof num1;
+  console.log(output1);
+
+  let sum = num + num1;
+  console.log(sum);
+  window.alert(sum)
+}
