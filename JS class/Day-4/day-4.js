221f391f81f06496903e944e1f4b9509c6da2013
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-03 12:24:07 +0530

    type casting in js

---
+++ JS class/Day-4/day-4.js
@@ -0,0 +1,14 @@
+// INPUT FROM THE USER AND FINDING ITS DATATYPE AND DOING THE DATA TYPE CONVERSION i.e. type casting 
+
+
+
+let num1 = window.prompt("Enter number 1st :-");
+let num2 = window.prompt("Enter numner 2nd :- ");
+
+
+window.alert(typeof(num1));
+window.alert(typeof(num2));
+
+let sum = parseFloat(num1)+ parseFloat(num2);
+
+window.alert(sum);
