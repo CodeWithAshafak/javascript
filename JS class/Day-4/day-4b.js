f9f86ce9f73f8786ab54871fb4ee1ed75f427de2
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-03 12:42:40 +0530

    find the greater number among three numbers in js

---
+++ JS class/Day-4/day-4b.js
@@ -0,0 +1,29 @@
+// find the greater number among three numbers in js
+
+let num1 =window.prompt("enter number")
+let num2 =window.prompt("enter number")
+let num3 =window.prompt("enter number")
+
+parseFloat(num1)
+parseFloat(num2)
+parseFloat(num2)
+
+
+if(num1>num2 && num1>num3)
+{
+    window.alert("number 1  is greater and the number is  ")
+   
+
+}
+else if (num2> num1 && num2>num3)
+{
+    window.alert("number 2 is greater and the number is ")
+    window.alert(num2)
+}
+else
+{
+    window.alert("number 3 is greater and the number is ")
+    window.alert(num3)
+}
+
+
