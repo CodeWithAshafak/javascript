c33b911a8b225b9c6b462d236e1d7f3970199f49
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-03 12:52:30 +0530

    Unlike strings or numbers, every symbol is unique, even if they have the same description.

---
+++ JS class/Day-5/Day-5.js
@@ -0,0 +1,10 @@
+// Unlike strings or numbers, every symbol is unique, even if they have the same description.
+
+// symbol data type khud me unique hota hai ,chahe wo same hi dusri bar declare karo wo unique hi hota hai 
+
+
+let sym1 = Symbol("Ashafak")
+let sym2 = Symbol ("Ashafak")
+
+
+console.log(sym1 == sym2)
