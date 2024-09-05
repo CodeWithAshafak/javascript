509183d8f9a9baa112361bd99ff08235db2f81f9
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-05 14:47:49 +0530

    onmouseover effect by using different images in one div and large imaage in other div

---
+++ JS class/Day-8/day-8a.js
@@ -0,0 +1,24 @@
+function change_img_one(){
+    let selectimg = document.querySelector('.largeimg')
+    selectimg.src = "/image/d-1.jpg"
+}
+function change_img_two(){
+    let selectimg = document.querySelector('.largeimg')
+    selectimg.src = "/image/d-2.jpg"
+}
+function change_img_three(){
+    let selectimg = document.querySelector('.largeimg')
+    selectimg.src = "/image/d-3.jpg"
+}
+function change_img_four(){
+    let selectimg = document.querySelector('.largeimg')
+    selectimg.src = "/image/d-4.jpg"
+}
+function change_img_five(){
+    let selectimg = document.querySelector('.largeimg')
+    selectimg.src = "/image/d-5.jpg"
+}
+function change_img_six(){
+    let selectimg = document.querySelector('.largeimg')
+    selectimg.src="/image/d-6.jpg"
+}
