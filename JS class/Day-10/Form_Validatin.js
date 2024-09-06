37ee23864d4c111f695a4fb6200d3dce3a976470
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-06 23:26:25 +0530

    form validation only basic code

---
+++ JS class/Day-10/Form_Validatin.js
@@ -0,0 +1,13 @@
+function run()
+
+{
+    // e.preventDefault();
+    let name = document.querySelector('.name').value;
+    let userName = document.querySelector('.username').value;
+
+    // console.log(name);
+    // console.log(userName);
+    window.alert(parseNumber name +userName)
+    return false;
+    
+}
