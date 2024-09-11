3fc0b14d3e5b7978f11355b891f4dbb3812d08de
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-11 23:01:29 +0530

    datatype done

---
+++ ChaiAndJS/Basic/02_datatypes.js
@@ -0,0 +1,56 @@
+"use strict"; // isse Javascript compailer newer vesion of javascripte use karega ...all code treat like new standard 
+
+
+//alert(3 +5 ) //we are using node.js , not browser if we write the same code in the browser console it will run q ki js ka engine browser ke andar chipa hua hota hai 
+
+
+console.log(6+
+    4)
+console.log("ashfak") // code readiblity should be high , ye code chal to jayega but we have to give proper semicolon and indentation also 
+
+
+// TYPES OF DATA TYPE :-
+
+let name = "ashafak";//
+console.log(typeof name);
+
+
+let age = 34;
+console.log(typeof age);
+
+let isLoggedIn = false
+console.log(typeof isLoggedIn);
+
+console.log(typeof "ashafk");
+
+
+
+// TYPE OF DATA TYPE  IN JS :-
+
+//  TYPE -1 :- PREMATIVE DATATYPES :-
+
+
+
+// 1>  NUMBER =>  range 2 to the power 
+// 2> BigInt
+// 3> String => " "
+// 4> Boolean => true/false
+// 5> null => stand alone value ,isse forcefully null raakhte hai , for example 
+      let state = null
+// 6> undefine => variable banay but value assign nhi kiya.
+      let c;
+// 7>  symbol = uniqueness ke bare me baat karne k liye symbol use karte hai 
+
+
+
+// TYPE -2: OBJECT
+
+console.log( typeof undefined) //  "undefined"  hi aayega ans
+console.log(typeof null );  //Ans will be " Object"
+
+
+
+
+
+
+
