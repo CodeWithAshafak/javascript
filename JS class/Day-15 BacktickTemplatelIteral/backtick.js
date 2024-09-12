0d619364760ad93135effe2d8edbd4aaa0b64d6f
ASTANWALE ASHAFAK ALTAF <143120421+Ashafak-7@users.noreply.github.com>
2024-09-12 22:58:46 +0530

    backtick basic 01

---
+++ JS class/Day-15 BacktickTemplatelIteral/backtick.js
@@ -0,0 +1,24 @@
+let heading  = "<h1>Hello</h1>";
+let paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptas, consectetur neque excepturi reprehenderit pariatur voluptates rerum delectus? Rerum, ullam?"
+
+
+/*
+let design1 = "<section>
+              <div></div>
+              <div></div>
+</section>"
+
+we can't store multiple lines of html in signle quote and dubble quote, for this we are using backtick 
+
+
+also used to display your answere on frontend 
+
+${} to access
+*/
+
+
+let design1 = `<section>
+
+        <div></div>
+        <div></div>
+</section>`
