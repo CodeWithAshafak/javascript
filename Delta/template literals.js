1efdb13c94f60e16a899068d11a26f46df58b62d
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-18 21:53:53 +0530

    updated

---
+++ Delta/template literals.js
@@ -0,0 +1,8 @@
+let pencilPrice = 10;
+let  eracerPrice = 5;
+/*let output = "the total price is : " + (pencilPrice + eracerPrice) + "Rupees";
+console.log(output)
+*/
+
+let output = `the total price is : ${pencilPrice + eracerPrice} Rupees`
+console.log(output);
