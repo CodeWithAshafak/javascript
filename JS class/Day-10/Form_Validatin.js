d5217099718814be7fe48c95817f9db38c370bef
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-18 21:20:32 +0530

    mdified code

--- JS class/Day-10/Form_Validatin.js
+++ JS class/Day-10/Form_Validatin.js
@@ -1,13 +1,15 @@
 function run()
-
 {
     // e.preventDefault();
-    let name = document.querySelector('.name').value;
-    let userName = document.querySelector('.username').value;
+    let n1 = document.querySelector('.number1').value;
+    let n2 = document.querySelector('.number2').value;
+
+    parseInt(n1);
+    parseInt(n2);
 
     // console.log(name);
     // console.log(userName);
-    window.alert(parseNumber name +userName)
+    window.alert(n1+n2);
     return false;
     
 }
