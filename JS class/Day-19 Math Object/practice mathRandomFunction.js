a9e33fbe64f17cf560406258e835269e4e99efd6
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-21 13:54:24 +0530

    Strong Password Generator

---
+++ JS class/Day-19 Math Object/practice mathRandomFunction.js
@@ -0,0 +1,21 @@
+//STRONG PASSWORD GENERATOR
+const generate = () => {
+
+   
+  let str = "1234567890asdfghjklASDFGHJKL&@#$%&*-";
+  blankStr = " ";
+  for(i=0 ;i<8;i++){
+    let saveRandomNumber = Math.random();
+    let multiply = str.length * saveRandomNumber;
+    let index = Math.floor(multiply);
+  
+    let code = str.charAt(index);
+    blankStr = blankStr + code;
+    
+
+  }
+ 
+
+  let display = document.querySelector(".password");
+  display.innerHTML = blankStr;
+};
