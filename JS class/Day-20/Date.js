ab694d7ff92dcee9a152a9296d9fa6fc5e350e04
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-21 23:33:55 +0530

    Date method

---
+++ JS class/Day-20/Date.js
@@ -0,0 +1,18 @@
+//Date is the premo class which is  present in browser 
+
+
+
+let obj = new Date();
+// new keywords  is used to create the object of the class,here obj is object of class Date  
+console.log(obj.getTime());
+console.log(obj.getFullYear());
+console.log(obj.getDay());
+
+
+
+console.log(obj.toISOString());
+console.log(obj.toLocaleDateString());
+console.log(obj.toString());
+console.log(obj.toTimeString());
+
+
