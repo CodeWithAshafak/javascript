5b452c294de31b390a3240dfbd743eba295ce48e
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-25 21:10:23 +0530

    indexOf method

---
+++ Delta/String/MethodsWithArgumentsIndexOf.js
@@ -0,0 +1,8 @@
+let s = "IloveCoding";
+let t=s.indexOf("love") // 1 ye love ka first index batata hai 
+console.log(t);
+ let p=s.indexOf("J") //-1 (not found)
+console.log(p);
+
+ let m = s.indexOf("o") // 2(gives only one index and it prefer to first one , in case 2 is prefered)
+console.log(m);
