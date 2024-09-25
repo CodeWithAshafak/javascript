b7f5c1ecb1849d523ade27cca6b391b4daac8e36
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-25 09:23:05 +0530

    Trim method

---
+++ Delta/TrimMethod.js
@@ -0,0 +1,14 @@
+//strig Method 
+
+let s = "       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum explicabo excepturi sapiente neque.       ";
+
+
+//trim() :- it removes whitespaces from start and end 
+console.log(s);
+console.log(s.trim());
+
+s = prompt("enter your password");
+console.log(s);
+ let newPassword =s.trim();
+ console.log(newPassword);
+ 
