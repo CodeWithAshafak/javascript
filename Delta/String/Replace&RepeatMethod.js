d6690142a7eea0b609c7460d297112d3b1b10c7c
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-27 09:50:04 +0530

    replace and repeat

---
+++ Delta/String/Replace&RepeatMethod.js
@@ -0,0 +1,14 @@
+let str = "ILoveCoding";
+let r = str.replace("Love","do");//yha "love" "do" se replace ho jayega.
+ console.log(r); //Ans :- IdoCoding
+
+
+ let m = str.replace('o', 'd');
+ console.log(m)
+
+// Repeat:- multiple copy deta hai 
+let msg = "ILoveCoding";
+t =msg.repeat(4);
+console.log(t)
+
+
