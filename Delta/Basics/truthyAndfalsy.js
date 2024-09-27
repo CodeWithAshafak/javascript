527c817bd2f6d61869f5e021169866e9f4145056
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-27 09:49:30 +0530

    Truthy and falsy

---
+++ Delta/Basics/truthyAndfalsy.js
@@ -0,0 +1,13 @@
+if (-0) {
+  console.log("this is true");
+} else {
+  console.log("this is false");
+}
+
+if ("") {
+  console.log("this is true");
+} else {
+  console.log("this is false");
+}
+
+// All the vlaue like false, 0, -0, on(Binintvalue)," ",null,undefined, NaN inke alava sab true hoga
