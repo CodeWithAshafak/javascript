6df93fc3b92110eb6fa969757ce2eba8235eba04
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-09-28 12:44:04 +0530

    basics of array

---
+++ Delta/Arrays/basicArray.js
@@ -0,0 +1,12 @@
+// ARRAY IS THE LINEAR DATA TYPE 
+let student1 = "aman";
+let student2 = "shardha";
+
+
+// but in array
+let students = ["ashafak","shardha", "rajat"];
+console.log(students);
+console.log(typeof(students) )
+// we can store different type of data in array
+let info = ["shardha",23,45.54]
+console.log( "info arrays lenght is ", info.length)
