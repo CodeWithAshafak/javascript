777e4ffe3054820736d6ba0b302e9c9b782aab7f
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-02 19:09:11 +0530

    module in js :- we are exporting the data to the another js file

---
+++ JS class/Day-28/main.js
@@ -0,0 +1,5 @@
+let num = 45;
+let arr = [34,4,4,3,6];
+export default num; // default export :- ager signle value hai tab hi use kar skte 
+
+ export{arr};//named export : multiple value ko ek hi saath use krane ke liye use karte
