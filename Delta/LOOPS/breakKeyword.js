d1019f47af29e4aca2a7a50779f4b8c9eeb92e3a
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-03 19:30:51 +0530

    break keyword

---
+++ Delta/LOOPS/breakKeyword.js
@@ -0,0 +1,14 @@
+// break is used to stop the conditon in the loop
+// kisi bhi conditon me se bhara nikalna hota hai tb break keyword ka use hota hai 
+// break maximum case me while loop ke sath use hota hai  
+let i=1;
+while(i<=5){
+    if(i==3)
+    {
+        break;
+    }
+    console.log(i);
+    i++;
+    
+}
+console.log("we are used break");
