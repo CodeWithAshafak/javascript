01649e0be1f8b482256d2c7cff58c6d9ca0d51e2
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-04 22:49:44 +0530

    nested FOR loop

---
+++ Delta/LOOPS/nested FOR OF loop.js
@@ -0,0 +1,13 @@
+let heroes = [ ["superman","batman","wonder-women"], ["spiderman","ironman","thor"]]
+
+for(list  of heroes)
+{
+    // console.log(`${i}`,i);
+    for(item of list)
+    {
+        console.log(item);
+        
+    }
+
+    
+}
