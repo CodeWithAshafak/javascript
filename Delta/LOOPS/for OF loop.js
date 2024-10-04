3cb8f79f478e2505f1443d03a8b8c46d0873e7ac
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-04 22:44:26 +0530

    for of loop

---
+++ Delta/LOOPS/for OF loop.js
@@ -0,0 +1,10 @@
+//usually it is used to acsess the collection  indidual item 
+// ex. if we want access the individual item of the array or string 
+
+let fruits = [ "mango", "apple","banana","orange","litchi"];
+
+for(fruit of fruits)
+{
+    console.log(fruit);
+    
+}
