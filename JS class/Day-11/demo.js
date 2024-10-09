216299e404212e6b5553b6f7f0bd38455e2ae54a
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-09 23:51:43 +0530

    grade system

---
+++ JS class/Day-11/demo.js
@@ -0,0 +1,9 @@
+
+
+function sum()
+
+{   let  display =  document.querySelector(".display");
+    let number =  document.querySelector(".number").value;
+     display.innerHTML = number;
+    console.log("good moringn")
+}
