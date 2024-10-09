6e3dc4f9dcc0c729123b55e671971716fc11679e
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-09 23:50:51 +0530

    update

---
+++ JS class/Day-10/validationPractice.js
@@ -0,0 +1,8 @@
+function submit()
+{
+    let n1 = parseInt( document.querySelector(".one").value);
+    let n2 = parseInt( document.querySelector(".two").value);
+
+    window.alert(n1+n2);
+    return false;
+}
