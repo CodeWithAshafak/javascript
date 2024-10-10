64dc772d15c1ee082361337040e1504a6077f4b1
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-10 23:03:26 +0530

    math.js

--- JS class/Day-19 Math Object/math.js
+++ JS class/Day-19 Math Object/math.js
@@ -1,7 +1,7 @@
 let val = 45.01;
 let ans = Math.ceil(val);
 let ans1 = Math.floor(val);
-let ans2 = Math.round(45.09);
+let ans2 = Math.round(45.3); //if the decemal value is below 5 then it turn to floor and if the deciaml value is greater than or equal to(= >.5 ) then it goes to ceil
 console.log(ans);
 console.log(ans1);
 console.log(ans2);
