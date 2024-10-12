595b0b6898e9105bc28418d10953e7ddcdfddc6c
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-12 23:17:11 +0530

    MethodOfJava

---
+++ JS class/Day-27 SpridOperatror/remainingMetodsOfArray.js
@@ -0,0 +1,16 @@
+let arr = [3,56,7,7,89,244]
+let arr1 = [3,56,7,244]
+console.log(arr);
+
+let strArray = arr.toString();
+console.log(strArray);
+console.log(typeof(strArray));
+
+
+let concatArray = arr.concat(arr1)
+console.log(concatArray)
+
+let joinArray = arr.join(" ");
+console.log(joinArray );
+console.log(typeof(joinArray));
+
