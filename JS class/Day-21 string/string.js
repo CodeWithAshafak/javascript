f95b36bc862e93466d87e9d912412d4dec5c2391
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-12 23:05:51 +0530

    string

---
+++ JS class/Day-21 string/string.js
@@ -0,0 +1,69 @@
+// type cohargan in js:-
+
+console.log(0.1+0.2);
+console.log('4'+4);
+console.log('4'-4);
+console.log(4+true);
+console.log(4+false);
+
+//================**********==================
+// string :-
+let str =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis mollitia rem magnam rerum molestiae libero, asperiores cum, necessitatibus animi, minus voluptates nesciunt id sint voluptatibus aperiam optio dolor m dolor sit amet consectetur adipisicing elit. Nobis mollitia rem magnam rerum molestiae libero, asperiores cum, necessitatibus animi, minus voluptates nesciunt id sint voluptatibus aperiam optio dolor odit veritatis! ";
+
+//  for(let i in str)
+// {
+//         console.log(i)
+
+// } // it iterate all the index value till last 
+
+
+//  for(let i of str)
+// {
+
+//     console.log(i)
+
+// }//give charecter at the specific index and here gives all the indexed charecter
+
+
+
+// ---------------
+
+// let storeStr = "";
+// for (let i of str) {
+//   if (i !== " ") {
+//     storeStr = storeStr + i;
+//   }
+// }
+
+// console.log(storeStr);
+
+
+
+
+
+// ---------------
+
+let storeStr = "";
+for (let i of str) {
+if (i !== "I") {
+     storeStr = storeStr + i;
+  }
+}
+
+console.log(storeStr);
+
+console.log(str.replaceAll(" ",""));
+console.log(str.replace(" ","")); // it replace only first space of string.
+console.log(str.length);
+
+
+
+let s = str.slice(34,90);
+console.log(s);
+console.log(str.indexOf())
+
+
+// ----------------------
+
+
