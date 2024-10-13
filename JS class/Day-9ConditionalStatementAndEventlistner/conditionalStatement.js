45f5ab8e8ac24b9edfa3ef49c9f23d476b0aa5c3
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-13 19:56:52 +0530

    update

---
+++ JS class/Day-9ConditionalStatementAndEventlistner/conditionalStatement.js
@@ -0,0 +1,41 @@
+function change_img(a){
+    console.log(a)
+    let selectimg = document.querySelector('.largeimg')
+
+    if(a==1)
+    {
+        selectimg.src = "/image/d-1.jpg"
+
+    }
+    else if(a==2)
+    {
+          selectimg.src = "/image/d-2.jpg"
+
+    }
+    else if(a==3)
+    {
+          selectimg.src = "/image/d-3.jpg"
+
+    }
+    else if(a==4)
+    {
+          selectimg.src = "/image/d-4.jpg"
+
+    }
+    else if(a==5)
+    {
+          selectimg.src = "/image/d-5.jpg"
+
+    }
+    else if(a==6)
+    {
+          selectimg.src = "/image/d-6.jpg"
+
+    }
+
+
+    
+
+
+    
+}
---
+++ JS class/project/login.js
@@ -0,0 +1,18 @@
+function submit()
+{
+    let login = document.querySelector('#id').value;
+    let pass = document.querySelector('#pass').value;
+
+    if(login===" ")
+    {  
+       window.alert("please enter login id")
+    }
+    else if(pass ===" ")
+    {
+    
+        
+        window.alert("please enter password")
+    }
+
+    return false;
+}
