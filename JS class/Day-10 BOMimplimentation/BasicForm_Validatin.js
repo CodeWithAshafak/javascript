cff4b8ca6c994c7ac7b3cff606e1c34b8208972a
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-13 19:52:01 +0530

    basic form validation

---
+++ JS class/Day-10 BOMimplimentation/BasicForm_Validatin.js
@@ -0,0 +1,15 @@
+function run()
+{
+    // e.preventDefault();
+    let n1 = parseInt(document.querySelector('#one').value);
+    let n2 = parseInt( document.querySelector('#two').value);
+
+   //parseInt(n1);
+   // parseInt(n2);
+
+    // console.log(name);
+    // console.log(userName);
+    window.alert(n1+n2);
+     return false;
+    
+}
