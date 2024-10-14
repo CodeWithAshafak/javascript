f2155db0ad407c2e430d7fe0bcf56ed48a70bb7f
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-14 22:48:33 +0530

    TEST-3

---
+++ JS class/TEST-3/TestAns/7.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program a box of size 560*700 When a mouse enters a box it converts into a circle.
+
+
+function change(){
+    let main = document.querySelector(".main");
+    main.style="border-radius:50%"
+} 
