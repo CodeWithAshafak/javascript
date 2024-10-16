b2b48c374748b87732a979956e015999d8b088ba
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-16 23:14:57 +0530

    demo

---
+++ JS class/Day-11 practice/demo.js
@@ -0,0 +1,8 @@
+
+
+function sum(){   
+    let  display =  document.querySelector(".display");
+    let number =  document.querySelector(".number").value;
+     display.innerHTML = number;
+    console.log("good moringn")
+}
