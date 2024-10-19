5578889e79034a14221493a57195be458ffde336
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-19 23:10:00 +0530

    update

---
+++ JS class/Day-33 JQ/jquery.js
@@ -0,0 +1,36 @@
+// $(document).ready(function(){
+
+
+//     // $("#btn").click(function(){
+//     //     $("#btn").css({"color":"red","font-size":"50px"})
+//     // });
+//       $('#btn').click(function(){
+//         $('#p').slideToggle();
+//       })
+
+// })
+
+
+$(document).ready(function(){
+    // $('#btn').click(function(){
+    //     $('#btn').css({"color":"red","font-size":"50px"})
+    // });
+
+    // ----------------------------------------
+//     $('#btn').click(function()
+// {
+//     $(this).css({"color":"red","font-size":"50px"})
+// })
+// =================================
+
+// $('#btn').click(function()
+//  {
+//      $('#p').toggle()})
+//  
+// =========================================
+
+$('#btn').click(function(){
+    $('#p').slideToggle();
+})
+
+})
