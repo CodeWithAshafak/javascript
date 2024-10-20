c3c35dcaa20424a6ea609c4a56423afe04c5da0b
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-20 22:33:05 +0530

    update

---
+++ JS class/TEST-3/TestAns/2.js
@@ -0,0 +1,8 @@
+// Write a JavaScript program when click on button image change.
+
+function change(){
+    let selectimg = document.querySelector("#img");
+    selectimg.src = "/image/airplane 3.jpg"
+
+ 
+}
